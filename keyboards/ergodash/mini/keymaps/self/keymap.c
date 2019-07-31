d9574af8424d4e195491b8b14e94e6b7d8da7bfe
awazoooo <awazo4662@gmail.com>
2019-07-31 12:47:50 +0900

    fix: remove MT

--- keyboards/ergodash/mini/keymaps/self/keymap.c
+++ keyboards/ergodash/mini/keymaps/self/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                       KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ESC,                      KC_BSPACE, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-    RESET,   ADJUST, KC_LALT, KC_LCMD,  KC_LSFT, LT(_LOWER, KC_SPC),  KC_SPC, CTL_T(KC_ENT), LT(_RAISE, KC_ENT), KC_LCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    RESET, ADJUST, KC_LALT, KC_LCMD,          KC_LSFT,     LOWER,  KC_SPC, CTL_T(KC_ENT),  RAISE, KC_LCTL, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* Lower
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * |   ~  |   F1 |   F2 |   F3 |   F4 |   F5 |   {  |                    |   }  |   1  |   2  |   3  |   4  |   5  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   F6 |   F7 |   F8 |   F9 |  F10 |   ,  |                    |  BSC |   6  |   7  |   8  |   9  |   0  | Shift|
+   * | Shift|   F6 |   F7 |   F8 |   F9 |  F10 |  ESC |                    |  BSC |   6  |   7  |   8  |   9  |   0  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
    * | Ctrl |  GUI |  ALt | Cmd  |||||||| Shift| Lower| Space|||||||| Enter| Raise|   <  ||||||||   ,  |   .  |   >  |   /  |
    * ,----------------------------------------------------------------------------------------------------------------------.
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_DQT, \
-    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_COMM,                      KC_BSPACE, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_RSFT, \
-    KC_NO,   KC_NO, KC_LALT, KC_LCMD,           KC_LSFT, MT(LOWER, KC_SPC), KC_SPC, CTL_T(KC_ENT), MT(RAISE, KC_ENT), KC_LT,        KC_COMM, KC_DOT, KC_GT, KC_SLSH \
+    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_ESC,                      KC_BSPACE, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_RSFT, \
+    KC_NO,   KC_NO, KC_LALT, KC_LCMD,           KC_LSFT,    LOWER,  KC_SPC, CTL_T(KC_ENT),  RAISE, KC_LT,        KC_COMM, KC_DOT, KC_GT, KC_SLSH \
   ),
 
   /* Raise
@@ -68,8 +68,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT(
     KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
     KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_LCBR,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_COLN, KC_DQT , \
-    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_ESC ,                        KC_BSPACE, KC_N,    KC_M,    KC_LT,   KC_GT, KC_QUES, KC_RSFT, \
-    KC_NO  , KC_F11 , KC_F12,  KC_LCMD,          KC_LSFT, MT(LOWER, KC_SPC),  KC_SPC, CTL_T(KC_ENT), MT(RAISE, KC_ENT), KC_LCTL,         KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
+    KC_LSFT, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_ESC,                      KC_BSPACE,    KC_N,    KC_M,   KC_LT,   KC_GT, KC_QUES, KC_RSFT, \
+    KC_NO  , KC_F11 , KC_F12,  KC_LCMD,          KC_LSFT,   LOWER, KC_SPC, CTL_T(KC_ENT),   RAISE, KC_LCTL,          KC_HOME, KC_PGDN, KC_PGUP, KC_END   \
   ),
 
   /* Adjust
